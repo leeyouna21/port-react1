97138a5b7bcba8b5abf20d63c8db88cfa4a408d7
leeyouna21 <yuna5845@gmail.com>
2023-07-17 22:20:46 +0900

    dfdf

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
 
 import Home from "./components/pages/Home";
 
@@ -13,7 +12,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,22 +2,12 @@ import React from "react";
 
 import Intro from "../section/Intro";
 import About from "../section/About";
-import Desc from "../section/Desc";
-import Scroll1 from "../section/Scroll1";
-import Desc2 from "../section/Desc2";
-import Scroll2 from "../section/Scroll2";
-import Contact from "../section/Contact";
 
 const Home = () => {
   return (
     <>
       <Intro />
       <About />
-      <Desc />
-      <Scroll1 />
-      <Desc2 />
-      <Scroll2 />
-      <Contact />
     </>
   );
 };
